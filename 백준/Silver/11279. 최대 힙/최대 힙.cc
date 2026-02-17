6112e564fac8b5c9ecbd3894c18298f21bac8162
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-02-17 22:07:44 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+void heap();
+void heap_del();
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	v.push_back(0);
+	
+	while(n--) {
+		int x;
+		cin >> x;
+		
+		if(x > 0) {
+			v.push_back(x);
+			heap();
+		}
+		
+		else if(x == 0) {
+			if(v.size() == 1) cout << 0 << "\n";
+			else {
+				cout << v[1] << "\n";
+				v[1] = v[v.size() - 1];
+				v.pop_back();
+				heap_del();
+			}
+		}	
+	}
+	
+	return 0;
+}
+
+void heap() {
+	int idx = v.size()-1;
+	while(idx > 1 && v[idx/2] < v[idx]) {
+		int t = v[idx];
+		v[idx] = v[idx/2];
+		v[idx/2] = t;
+		idx /= 2;
+	}
+}
+
+void heap_del() {
+	int idx = 2;
+	while(idx <= v.size() - 1) {
+		if(idx != v.size() - 1) {
+			if(v[idx] < v[idx + 1]) idx += 1;
+		}
+		if(v[idx/2] < v[idx]) {
+			int t = v[idx];
+			v[idx] = v[idx/2];
+			v[idx/2] = t;
+			idx *= 2;		
+		}
+		else break;
+	}	
+}
