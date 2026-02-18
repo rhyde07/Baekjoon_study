53366db7dddeea87e3ff074cd18fcb0673d5258b
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-02-18 23:47:35 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d,e;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	scanf("%d", &d);
+	scanf("%d", &e);
+	printf("%d", a+b+c+d+e);
+	
+	return 0;
+}
