36aa7fe122e50e59ed56d8e0309bcfce20acb5bc
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-02-18 23:42:53 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int a,b,c,d,e;
+	scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+	int sum = pow(a,2) + pow(b,2) + pow(c,2) + pow(d,2) + pow(e,2);
+	printf("%d", sum%10);
+	
+	return 0;
+}
