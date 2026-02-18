39efb3d8ebfe63652532f35f9ef0cca04f73442e
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-02-18 13:17:41 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	
+	while(n--) {
+		cout << "SciComLove" << "\n";
+	}
+	
+	return 0;
+}
