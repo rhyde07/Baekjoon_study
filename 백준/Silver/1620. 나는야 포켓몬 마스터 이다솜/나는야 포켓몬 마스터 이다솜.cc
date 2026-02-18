3a69189dc2a4f0bd72dc2d68d1ba2cdb61af24e6
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-02-18 23:41:10 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 88 ms, Memory: 20660 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <ctype.h>
+
+using namespace std;
+unordered_map<string, int> aNum;
+string aName[100002];
+	
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n, m;
+	cin >> n >> m;
+	 
+	
+	for(int i = 1; i <= n; i++) {
+		string l;
+		cin >> l;
+		aNum.insert({l, i});
+		aName[i] = l;
+	}
+	
+	for(int i = 0; i < m; i++) {
+		string l;
+		cin >> l;
+		
+		if(isdigit(l[0])) {
+			int t = stoi(l);
+			cout << aName[t] << "\n";
+		}
+		else cout << aNum[l] << "\n";
+	}
+	
+	return 0;
+}
