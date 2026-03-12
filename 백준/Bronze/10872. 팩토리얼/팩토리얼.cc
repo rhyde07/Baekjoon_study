2b4602fd88603eb94b38facb34cf5e646759d8a0
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-03-12 22:36:01 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n);
+int main(){
+	
+	int n;
+	cin >> n;
+	cout << factorial(n);
+	
+	return 0;
+}
+
+int factorial(int n) {
+	if(n == 0) return 1;
+	else return factorial(n-1) * n;
+}
