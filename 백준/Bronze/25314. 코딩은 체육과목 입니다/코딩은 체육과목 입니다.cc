17f0b8dd086c49acd08195e7a974c58362361950
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-03-20 22:40:52 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    n /= 4;
+    
+    while(n--) {
+        cout << "long ";
+    }
+    cout << "int";
+    
+    return 0;
+}
